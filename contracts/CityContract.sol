fba82b801369048dcf8804b4bee4ebacf23a5dd0
tzhu <alistairzhu@gmail.com>
2018-03-30 21:52:02 -0400

    formating

--- contracts/CityContract.sol
+++ contracts/CityContract.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
 import "./WonderToken.sol";
-import "./ConvertLib.sol"; //Zhu
+import "./ConvertLib.sol";
 
 contract CityContract is WonderToken{
 
@@ -19,10 +19,9 @@ contract CityContract is WonderToken{
     WonderStruct public wonder;
 
     mapping (address => WonderStruct) wonders;
-    mapping (address => uint) balances; // Zhu
+    mapping (address => uint) balances;
 
     address[] public wonderAddressArray;
-
     uint256 public wonderID =0 ;
 
     event wonderInfo(
@@ -35,9 +34,9 @@ contract CityContract is WonderToken{
       bytes32 field_5
     ) ;
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value); // Zhu
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {  //Zhu
+    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
       if (balances[msg.sender] < amount) return false;
       balances[msg.sender] -= amount;
       balances[receiver] += amount;
@@ -50,27 +49,23 @@ contract CityContract is WonderToken{
     }
 
     function setWonderStruct(address _address, bytes32 _field_1, bytes32 _field_2, bytes32 _field_3, bytes32 _field_4, bytes32 _field_5) public {
-
         wonder = wonders[_address];
         wonderID += 1;
         wonder.addr = _address;
         wonder.id = wonderID;
-
         wonder.field_1 = _field_1;
         wonder.field_2 = _field_2;
         wonder.field_3 = _field_3;
         wonder.field_4 = _field_4;
         wonder.field_5 = _field_5;
-
         wonderAddressArray.push(_address) -1;
-
         WonderStructArray.push(wonder) -1;
-        wonderInfo(_address,wonderID, _field_1, _field_2, _field_3, _field_4, _field_5) ;
 
+        wonderInfo(_address,wonderID, _field_1, _field_2, _field_3, _field_4, _field_5) ;
     }
 
     function getWonderStructs() view public returns (address[]) {
-            return (wonderAddressArray);
+        return (wonderAddressArray);
     }
 
     function getWonderStructByID(uint256 i) view public returns (address, uint256 , bytes32 , bytes32 , bytes32) {
@@ -78,13 +73,10 @@ contract CityContract is WonderToken{
     }
 
     function getWonderStructByAddress(uint256 i, address ins) view public returns (address, uint256 , bytes32 , bytes32 , bytes32) {
-
         if (ins == WonderStructArray[i].addr){
             return (WonderStructArray[i].addr,WonderStructArray[i].id, WonderStructArray[i].field_4, WonderStructArray[i].field_1, WonderStructArray[i].field_2);
         }
-
     }
-
 
     function getlastWonderStruct() view public returns (address, uint256 , bytes32 , bytes32 , bytes32, bytes32, bytes32) {
             return (wonder.addr,wonder.id,wonder.field_1,wonder.field_2,wonder.field_3,wonder.field_4,wonder.field_5  );
